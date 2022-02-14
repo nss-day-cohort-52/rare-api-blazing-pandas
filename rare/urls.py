768d30a2a20e1d8929f9ddf46bd4b7a7157a4eac
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-02-14 12:14:38 -0700

    added login out and register functinality

--- rare/urls.py
+++ rare/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
+from rareapi.views import login_user, register_user
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+
 
 urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
---
+++ rareapi/models/__init__.py
@@ -0,0 +1,5 @@
+from .tag import Tag
+from .post import Post
+from .rareuser import RareUser
+from .category import Category
+from .postTag import PostTag
