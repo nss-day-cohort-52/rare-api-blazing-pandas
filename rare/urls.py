6d669dbd6c7360d5e3d3108ebc36951111dd8e6a
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-02-15 08:44:09 -0700

    implemented list and retrieve method, models,fixtures, and relevant urls for categories

--- rare/urls.py
+++ rare/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
 from rareapi.views import login_user, register_user
+from rareapi.views.categories import CategoryView
 
 
 router = routers.DefaultRouter(trailing_slash=False)
+
+router.register(r'categories', CategoryView, 'category')
 
 
 urlpatterns = [
