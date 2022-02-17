3eab50419a37c7b0ad5c74a6c85589bed8ca01e3
auntviv <aviva.mondesir@gmail.com>
2022-02-17 15:36:56 -0600

    init

--- rareapi/views/__init__.py
+++ rareapi/views/__init__.py
@@ -1,12 +1,6 @@
 from rareapi.views.post import PostView
-<<<<<<< HEAD
-from .auth import register_user, login_user
-from rareapi.views.tag import TagView
-from.post import PostView
-from .tag import TagView
-=======
 from .auth import register_user, login_user, check_admin
 from.post import PostView
 from .rareusers import RareUserView
 from .categories import CategoryView
->>>>>>> main
+from .tag import TagView
